17b3e2f5ce9c3cd127e2b40ba79a24006d1de76e
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2016-08-10 05:33:35 +0200

    Encapsulates RK4 solver in class, starts tests.

--- Software/RK4/rk4.py
+++ Software/RK4/rk4.py
@@ -8,131 +8,95 @@ import numpy as np
 from pycuda import driver, compiler, gpuarray, tools
 import jinja2
 from progress_lib import progress_bar_init
-import os
-import time
-import h5py
 
 # When importing this module we are initializing the device.
 # Now, we can call the device and send information using
 # the apropiate tools in the pycuda module.
 import pycuda.autoinit
 
-# ================================ CONSTANTS ================================ #
 
-INIT_W = 2
-INIT_H = 1
-NUM_STEPS = 100
-STEP_SIZE = 0.02
+class RK4Solver:
+    def __init__(self, x0, y0, dx, systemFunctions):
+        """Builds a RungeKutta solver of 4h order"""
 
-CONSTANT_A = 25
-SYSTEM_SIZE = 2
+        # ============================ CONSTANTS ============================ #
 
-OUT_DIR = "./Output"
+        # Shape of the initial conditions matrix: width and height. This shape
+        # will define the dimensions of the CUDA grid
+        self.INIT_H = y0.shape[0]
+        self.INIT_W = y0.shape[1]
 
-# ======================== KERNEL TEMPLATE RENDERING ======================== #
+        # Number of equations on the system
+        self.SYSTEM_SIZE = y0.shape[2]
 
-# We must construct a FileSystemLoader object to load templates off
-# the filesystem
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
+        # Size of the step
+        self.STEP_SIZE = np.float32(dx)
 
-# An environment provides the data necessary to read and
-# parse our templates.  We pass in the loader object here.
-templateEnv = jinja2.Environment(loader=templateLoader)
+        # System function
+        self.F = [(str(i), f) for i, f in enumerate(systemFunctions)]
 
-# Read the template file using the environment object.
-# This also constructs our Template object.
-template = templateEnv.get_template("rk4_kernel.cu")
+        # ======================= INITIAL CONDITIONS ======================= #
 
-# Specify any input variables to the template as a dictionary.
-templateVars = {
-    "CONSTANT_A": CONSTANT_A,
-    "SYSTEM_SIZE": SYSTEM_SIZE
-}
+        self.x0 = np.float32(x0)
+        self.y0 = y0.astype(np.float32)
 
-# Finally, process the template to produce our final text.
-kernel = template.render(templateVars)
+        # ==================== KERNEL TEMPLATE RENDERING ==================== #
 
-# =========================== KERNEL COMPILATION =========================== #
+        # We must construct a FileSystemLoader object to load templates off
+        # the filesystem
+        templateLoader = jinja2.FileSystemLoader(searchpath="./")
 
-# Compile the kernel code using pycuda.compiler
-mod = compiler.SourceModule(kernel)
+        # An environment provides the data necessary to read and
+        # parse our templates.  We pass in the loader object here.
+        templateEnv = jinja2.Environment(loader=templateLoader)
 
-# Get the kernel function from the compiled module
-rungeKutta4 = mod.get_function("rungeKutta4")
+        # Read the template file using the environment object.
+        # This also constructs our Template object.
+        template = templateEnv.get_template("rk4_kernel.cu")
 
-# ============================== DATA LOADING ============================== #
+        # Specify any input variables to the template as a dictionary.
+        templateVars = {
+            "SYSTEM_SIZE": self.SYSTEM_SIZE,
+            "SYSTEM_FUNCTIONS": self.F
+        }
 
-# Load initial conditions
-# initConditions = np.random.rand(INIT_W, INIT_H, SYSTEM_SIZE+1)
-initConditions = np.array([[[-1, 1, 1]], [[-1, 2, 2]]], dtype=np.float32)
+        # Finally, process the template to produce our final text.
+        kernel = template.render(templateVars)
 
-# Coefficient matrix that defines the linear system
-systemCoeffs = np.array([[0, 1],
-                         [-CONSTANT_A, 0]])
+        print(kernel)
 
-# Array
-cond_cpu = initConditions.astype(np.float32)
-coeff_cpu = systemCoeffs.astype(np.float32)
+        # ======================= KERNEL COMPILATION ======================= #
 
-# Transfer host (CPU) memory to device (GPU) memory
-cond_gpu = gpuarray.to_gpu(cond_cpu)
-coeff_gpu = gpuarray.to_gpu(coeff_cpu)
+        # Compile the kernel code using pycuda.compiler
+        mod = compiler.SourceModule(kernel)
 
-# ============================= DATA CONTAINER ============================= #
+        # Get the kernel function from the compiled module
+        self.RK4Solve = mod.get_function("RK4Solve")
 
-file_name = "rk4.hdf5"
-full_path = os.path.join(OUT_DIR, file_name)
+        # ========================== DATA TRANSFER ========================== #
 
-hdf_root = h5py.File(full_path, "w")
+        # Transfer host (CPU) memory to device (GPU) memory
+        self.y0GPU = gpuarray.to_gpu(self.y0)
 
-# ============================ ACTUAL PROCESSING ============================ #
+    def solve(self):
+        # Call the kernel on the card
+        self.RK4Solve(
+            # Inputs
+            self.x0,
+            self.y0GPU,
+            self.STEP_SIZE,
 
-# Create two timers to measure the time
-start = driver.Event()
-end = driver.Event()
+            # Grid definition -> number of blocks x number of blocks.
+            # Each block computes one RK4 step for a single initial condition
+            grid=(self.INIT_W, self.INIT_H, 1),
+            # block definition -> number of threads x number of threads
+            # Each thread in the block computes one RK4 step for one equation
+            block=(self.SYSTEM_SIZE, 1, 1),
+        )
 
-start.record()  # start timing
+        # Update the time in which the system solution is computed
+        self.x0 = self.x0 + self.STEP_SIZE
 
-# progress_bar = progress_bar_init(NUM_STEPS-1)
-
-for step in range(NUM_STEPS):
-    # Start time measure
-    pb_start = time.time()
-
-    # Call the kernel on the card
-    rungeKutta4(
-        # inputs
-        cond_gpu,
-        coeff_gpu,
-        np.float32(STEP_SIZE),
-
-        # Grid definition -> number of blocks x number of blocks.
-        # Each block computes one RK4 step for a single initial condition
-        grid=(INIT_W, INIT_H, 1),
-        # block definition -> number of threads x number of threads
-        # Each thread in the block computes one RK4 step for one equation
-        block=(SYSTEM_SIZE, 1, 1),
-    )
-
-    # Copy back the results (the array returned by PyCUDA has the same shape as
-    # the one previously sent)
-    cond_cpu = cond_gpu.get()
-
-    print(cond_cpu)
-
-    # # Create file in HDF5 system
-    # hdf_root.create_dataset("out_%03d.csv" % step,
-    #                         data=cond_cpu,
-    #                         compression="gzip",
-    #                         compression_opts=9)
-
-    # End time measure and update progress bar
-    pb_end = time.time()
-    # progress_bar(pb_end - pb_start)
-
-end.record()    # end timing
-
-# calculate the run length
-end.synchronize()
-secs = start.time_till(end)*1e-3
-print("Finished in", secs, "seconds.")
+        # Return the new data
+        y1 = self.y0GPU.get()
+        return(y1)
--- Software/RK4/rk4_kernel.cu
+++ Software/RK4/rk4_kernel.cu
@@ -1,17 +1,10 @@
-#define CONSTANT_A      {{ CONSTANT_A }}
-#define SYSTEM_SIZE     {{ SYSTEM_SIZE }}
-
-// Global pointer to the system coefficients
-__device__ float* globalSystemCoeffs;
-
-// Auxiliar array to store the shared computations
-__shared__ float newValue[SYSTEM_SIZE];
+#define SYSTEM_SIZE {{ SYSTEM_SIZE }}
 
 /**
  * Returns the block identifier in a 2D grid
  * @return int Block identifier in which the running thread resides
  */
-__device__ int getBlockId(){
+__device__ inline int getBlockId(){
    return blockIdx.x + blockIdx.y * gridDim.x;
 }
 
@@ -19,7 +12,7 @@ __device__ int getBlockId(){
  * Returns the thread identifier in a 2D block
  * @return int Thread identifier local to the block
  */
-__device__ int getThreadId(){
+__device__ inline int getThreadId(){
     return (threadIdx.y * blockDim.x) + threadIdx.x;
 }
 
@@ -30,32 +23,25 @@ __device__ int getThreadId(){
  * @param float* y  Initial conditions
  * @param float* f  Computed value of the function
  */
-__device__ void computeComponent(int comp, float t, float* y, float* f){
-    int row = comp*SYSTEM_SIZE;
-
-    f[comp] = 0;
-
-    for(int i=0; i<SYSTEM_SIZE; i++){
-        f[comp] += globalSystemCoeffs[row + i] * y[i];
+__device__ void computeComponent(int threadId, float x, float* y, float* f){
+    switch(threadId) {
+        {% for i, function in SYSTEM_FUNCTIONS %}
+            case {{ i }}:
+                f[threadId] = {{ function }};
+                break;
+        {% endfor %}
     }
 }
 
-__global__ void rungeKutta4(void *devInitCond, void *devSystemCoeffs,
-                            float step){
-    // Pointers to the positions and accelerations
-    float* globalInitCond = (float*)devInitCond;
-    globalSystemCoeffs = (float*)devSystemCoeffs;
-
-    // Retrieve the identifier of the thread in the block and the block
-    // identifier
+__global__ void RK4Solve(float x0, void *devInitCond, float dx){
+    // Retrieve the identifier of the thread in the block
     int threadId = getThreadId();
     int blockId = getBlockId();
 
-    // Initial conditions used by the block
-    // TODO: Copy to shared memory
-    float* initConditions = globalInitCond + blockId*(SYSTEM_SIZE + 1);
-    float t = initConditions[0];
-    float* y = initConditions + 1;
+    // Pointers to the initial conditions
+    float* globalInitCond = (float*)devInitCond;
+    float* y0 = globalInitCond + blockId*SYSTEM_SIZE;
+
 
     // Auxiliar computations
     __shared__ float k1[SYSTEM_SIZE],
@@ -63,38 +49,35 @@ __global__ void rungeKutta4(void *devInitCond, void *devSystemCoeffs,
                      k3[SYSTEM_SIZE],
                      k4[SYSTEM_SIZE];
 
+    // New value
+    __shared__ float y1[SYSTEM_SIZE];
 
     // K1 computation
-    computeComponent(threadId, t, y, k1);
+    computeComponent(threadId, x0, y0, k1);
     __syncthreads();
 
     // K2 computation
-    newValue[threadId] = y[threadId] + step*k1[threadId]/2.0;
+    y1[threadId] = y0[threadId] + 0.5*dx*k1[threadId];
     __syncthreads();
 
-    computeComponent(threadId, t + step/2.0, newValue, k2);
+    computeComponent(threadId, x0 + 0.5*dx, y1, k2);
     __syncthreads();
 
     // K3 computation
-    newValue[threadId] = y[threadId] + step*k2[threadId]/2.0;
+    y1[threadId] = y0[threadId] + 0.5*dx*k2[threadId];
     __syncthreads();
 
-    computeComponent(threadId, t + step/2.0, newValue, k3);
+    computeComponent(threadId, x0 + 0.5*dx, y1, k3);
     __syncthreads();
 
     // K4 computation
-    newValue[threadId] = y[threadId] + step*k3[threadId];
+    y1[threadId] = y0[threadId] + dx*k3[threadId];
     __syncthreads();
 
-    computeComponent(threadId, t + step, newValue, k4);
+    computeComponent(threadId, x0 + dx, y1, k4);
     __syncthreads();
-
-    // Update time (do it just once)
-    if(threadId == 0)
-        initConditions[0] += step;
 
     // Update system value
-    initConditions[threadId + 1] += step*(k1[threadId] +
-                                          2*(k2[threadId]+k3[threadId]) +
-                                          k4[threadId])/6;
+    y0[threadId] += dx*(k1[threadId] + 2*(k2[threadId]+k3[threadId]) +
+                        k4[threadId])/6;
 }
