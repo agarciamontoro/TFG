4e1c3b29eb1db2228728e85397af22bb75e06bcb
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2016-12-08 20:03:30 +0100

    Adds textures and changes path on congruence.py

--- Software/Stuff/congruence.py
+++ Software/Stuff/congruence.py
@@ -24,23 +24,22 @@ camera1 = Camera(camR, camTheta, camPhi, camFocalLength, camSensorShape,
 camera2 = Camera(camR+10, camTheta, camPhi, camFocalLength, (500, 500),
                  camSensorSize)
 
-# # Make an sliced shoot; i.e., store all the intermediate steps in order to
-# # plot a 3D scene
-# plot3D = camera1.slicedShoot(slicesNum=10)
-# plot3D.plot()
-#
-# # Plot only one geodesic, indexing it with the pixel row,col
-# plot3D.geodesic(5, 5).plot()
-#
-# # You can even plot a snapshot, which may be not that interesting, though...
-# plot3D.snapshot(1).plot()
+# Make an sliced shoot; i.e., store all the intermediate steps in order to
+# plot a 3D scene
+plot3D = camera1.slicedShoot(slicesNum=10)
+plot3D.plot()
+
+# Plot only one geodesic, indexing it with the pixel row,col
+plot3D.geodesic(5, 5).plot()
+# You can even plot a snapshot, which may be not that interesting, though...
+plot3D.snapshot(1).plot()
 
 # Make a proper photography!
 photo = camera2.shoot()
 photo.plot()
 
 # Load the textures
-disk = '../Res/Textures/adisk.png'
-sphere = '../Res/Textures/milkyWay.png'
+disk = '../../Data/Textures/adisk.png'
+sphere = '../../Data/Textures/milkyWay.png'
 texturedImage = camera2.shoot(diskPath=disk, spherePath=sphere)
 texturedImage.plot()
